9283a265dda499767abfbe3cb85c5792b011b1bb
Wikram-12359 <wikramkumar16@gmail.com>
2026-06-17 14:04:14 +0500

    Update EnrolledCourses.tsx

--- src/components/student/EnrolledCourses.tsx
+++ src/components/student/EnrolledCourses.tsx
@@ -15,17 +15,12 @@ import { AlertCircle } from 'lucide-react';
 
 type ICourses = { _id: string, active: boolean, title: string; description: string };
 const EnrolledCourses = ({data}: {data: ICourses[]}) => {
-  // const courses: ICourses[] = [
-  //   {
-  //     title: 'ECAT Course',
-  //     description: 'this is a ecat course',
-  //   },
-  // ];
+  const courses = data ?? []
   const router = useRouter()
   return (
     <div>
       <div className='flex px-4 space-x-3 flex-wrap'>
-        {data.length == 0 ? (
+        {courses.length == 0 ? (
           <Card className="border-blue-200 bg-blue-50">
             <CardHeader>
               <CardTitle className="flex items-center gap-2 text-blue-900">
@@ -41,7 +36,7 @@ const EnrolledCourses = ({data}: {data: ICourses[]}) => {
             </CardContent>
           </Card>
         ):(
-          data?.map((e: ICourses) => (
+          courses?.map((e: ICourses) => (
           <Card key={e.title} className='w-60 @container/card'>
             <CardHeader>
               <CardDescription>{e.title}</CardDescription>
